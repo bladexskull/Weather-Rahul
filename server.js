7e34883dd626ffb821e8b73766d453e3c07b5ac6
bladexskull <bhagatrahul731@gmail.com>
2021-03-03 21:16:47 +0530

    added heroku postbuild

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-const express = require('express');
-const app = express();
+// function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
+// const express = require('express');
 // const app = express();
-app.use(requireHTTPS);
+// // const app = express();
+// app.use(requireHTTPS);
 
+// app.use(express.static('./dist/firstlearningapp'));
+
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/firstlearningapp/'}),
+// );
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/firstlearningapp'));
 
 app.get('/*', (req, res) =>

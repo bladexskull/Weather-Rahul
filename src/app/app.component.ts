552301f8ad749851179c29f820a574dbb89b23f2
bladexskull <bhagatrahul731@gmail.com>
2021-03-03 21:01:19 +0530

    heroku dep first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'firstlearningapp';
+  isTrue:boolean=true;
+  count:number=1;
+  myInterval:any;
+  maketrue() {
+   this.isTrue=true;
+  };
+  makefalse() {
+    this.isTrue=false;
+  }
+  increaseCount(){
+    this.count++;
+  }
+  regularCount(){
+    this.myInterval=setInterval(()=>{this.increaseCount();},0);
+  }
+stop(){
+  clearInterval(this.myInterval);
+  // this.count=0;
 }
+} 
